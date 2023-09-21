0003964f7dc361a44e74626495483fd64091cbf7
NartPacarizi <kujtesar_paqarizi@hotmail.com>
2023-09-21 21:13:19 +0200

    sound updated

--- scripts.js
+++ scripts.js
@@ -11,6 +11,58 @@ function myFunction() {
   }
 }
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var bgAudio = document.getElementById("bgAudio");
+  var muteButton = document.getElementById("muteButton");
+  var video = document.getElementById("video1");
+  var cookiesPolicyButton = document.getElementById("cookiesPolicyButton");
+  
+
+
+  function startVideo() {
+    video.play();
+    // Hide the Cookies Policy button after clicking
+    cookiesPolicyButton.style.display = "none";
+}
+
+// Add click event to the Cookies Policy button
+cookiesPolicyButton.addEventListener("click", startVideo);
+
+  // Function to toggle unmute/mute
+  function toggleMute() {
+      if (bgAudio.muted) {
+          // Unmute
+          bgAudio.muted = false;
+          muteButton.innerText = "Mute Music";
+      } else {
+          // Mute
+          bgAudio.muted = true;
+          muteButton.innerText = "Unmute Music";
+      }
+  }
+
+  // Autoplay the background music only after the cookies policy is accepted
+  cookiesPolicyButton.addEventListener("click", function () {
+    bgAudio.muted = false;
+    bgAudio.play();
+    // Hide the cookies policy button after accepting
+    cookiesPolicyButton.style.display = "none";
+  });
+
+  function startBackgroundMusic() {
+    setTimeout(function () {
+      bgAudio.muted = false;
+      bgAudio.play();
+    }, 3000); // Delay in milliseconds (5 seconds in this example)
+  }
+  // Add click event to the mute/unmute button
+  muteButton.addEventListener("click", toggleMute);
+});
+
+
 /** //var vid = document.getElementById("video1");
 
 var video = document.getElementById("video1");
@@ -119,6 +171,8 @@ var myScrollFunc = function () {
 };
 
 window.addEventListener("scroll", myScrollFunc);
+
+
 
 
 
